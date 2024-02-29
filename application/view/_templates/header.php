aed9b0904f450c1e88fcbe9bbaabbc2bb7170bdc
Thomas Plasonik <thomas.plasonik@siemens.com>
2024-02-29 15:15:14 +0100

    Register nur für Admin

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -36,9 +36,6 @@
                 <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
                 </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
             <?php } ?>
         </ul>
 
@@ -73,6 +70,11 @@
                     echo ' class="active" ';
                 } ?> >
                     <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
+                </li>
+                <li <?php if (View::checkForActiveController($filename, "register/index")) {
+                    echo ' class="active" ';
+                } ?> >
+                    <a href="<?php echo Config::get('URL'); ?>admin/">Register</a>
                 </li>
             <?php endif; ?>
         <?php endif; ?>
